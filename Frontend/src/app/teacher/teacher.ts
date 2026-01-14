1f22e55edabb4e7700c37ca864b7cd4398c88ab3
Aseem Aslah <aseemaslah4508@gmail.com>
2026-01-14 10:43:19 +0530

    latest push

--- Frontend/src/app/teacher/teacher.ts
+++ Frontend/src/app/teacher/teacher.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Teachersidebar } from "../teachersidebar/teachersidebar";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-teacher',
-  imports: [Teachersidebar],
+  imports: [Teachersidebar, RouterLink],
   templateUrl: './teacher.html',
   styleUrl: './teacher.scss',
 })
