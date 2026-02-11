03fef2aff3e20dd24d6553c8868d9bd75b9d11f5
Aseem Aslah <aseemaslah4508@gmail.com>
2026-02-11 11:31:16 +0530

    feat: Implement initial login page with styling and views for managing admins, teachers, and students.

--- Frontend/src/app/main/main.ts
+++ Frontend/src/app/main/main.ts
@@ -1,10 +1,9 @@
 import { Component, inject } from '@angular/core';
-import { Navbar } from "../navbar/navbar";
 import { Footer } from '../footer/footer';
-import { Router, RouterLink } from "@angular/router";
+import { Router } from "@angular/router";
 import { AuthService } from '../services/auth.service';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
@@ -19,27 +18,37 @@ export class Main {
   password: string = '';
   showPassword: boolean = false;
 
+  errorMessage: string = '';
+  loading: boolean = false;
+
   private authService = inject(AuthService);
   private router = inject(Router);
 
-  login() {
+  login(form: NgForm) {
+
+    if (form.invalid) return;
+
+    this.errorMessage = '';
+    this.loading = true;
+
     this.authService.login(this.username, this.password)
       .subscribe({
         next: (res) => {
-          console.log('Login response:', res);
-          if (res && res.user && res.user.role) {
+          this.loading = false;
+
+          if (res?.user?.role) {
             const role = res.user.role;
+
             if (role === 'Admin') this.router.navigate(['/admin']);
             if (role === 'Teacher') this.router.navigate(['/teacher']);
             if (role === 'Student') this.router.navigate(['/student']);
           } else {
-            console.error('Invalid response structure:', res);
-            alert('Login failed - invalid response');
+            this.errorMessage = 'Invalid response from server';
           }
         },
-        error: (error) => {
-          console.error('Login error:', error);
-          alert('Invalid credentials');
+        error: () => {
+          this.loading = false;
+          this.errorMessage = 'Invalid username or password';
         }
       });
   }
